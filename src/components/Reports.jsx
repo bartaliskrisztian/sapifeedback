a558ef79b848e219d135bcdc71ed63c85fab578c
Bartalis Krisztian <bartalis.krisztian@yahoo.com>
2021-03-10 15:41:55 +0200

    add pagination for reports table

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 // importing language resource file
 import stringRes from "../resources/strings";
@@ -13,6 +13,40 @@ function Reports(props) {
     // string resources
     let language = process.env.REACT_APP_LANGUAGE;
     let strings = stringRes[language];
+
+    const reportsToShow = 5;
+    const [currentPage, setCurrentPage] = useState(0);
+    const [pages, setPages] = useState([]);
+
+    useEffect(() => {
+        let p = [];
+        while(props.reports.length > 0) {
+            let chunk = props.reports.splice(0, reportsToShow);
+            p.push(chunk);
+        }
+        setPages(p);
+        // eslint-disable-next-line
+    }, []);
+
+    const firstPage = () => {
+        setCurrentPage(0);
+    }
+
+    const lastPage = () => {
+        setCurrentPage(pages.length - 1);
+    }
+
+    const nextPage = () => {
+        if(currentPage < pages.length-1) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+    const beforePage = () => {
+        if(currentPage > 0 ) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
     
     const ReportTableRow = () => {
         return (
@@ -22,7 +56,7 @@ function Reports(props) {
                         <th className='topic-reports__header text'>{strings.topic.reports.reportText}</th>
                         <th className='topic-reports__header image'>{strings.topic.reports.reportImage}</th>
                     </tr>
-              {props.reports && props.reports.map((report) => (
+              {props.reports && pages.length > 0 && pages[currentPage].map((report) => (
                 <tr key={report.date} className="topic-reports__row">
                     <td className="topic-reports__cell-text">
                         <div className="topic-reports__text" >
@@ -53,6 +87,12 @@ function Reports(props) {
     return (
         <div className="topic-reports__container">
             <ReportTableRow />
+            <div className="topic-reports__pagination-container">
+                <div className="topic-reports__pagination-element" onClick={firstPage}>Első</div>
+                <div className="topic-reports__pagination-element" onClick={beforePage}>Előző</div>
+                <div className="topic-reports__pagination-element" onClick={nextPage}>Következő</div>
+                <div className="topic-reports__pagination-element" onClick={lastPage}>Utolsó</div>
+            </div>
         </div>
     );
 }
