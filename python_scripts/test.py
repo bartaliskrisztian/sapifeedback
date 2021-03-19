b646d31ff73772fbb02af5f992bd15c46f0e8817
Bartalis Krisztian <bartalis.krisztian@yahoo.com>
2021-03-19 20:38:23 +0200

    installing python modules

--- python_scripts/test.py
+++ python_scripts/test.py
@@ -1,17 +1,15 @@
 import sys
 import json
 
-# Read data from stdin
-
 
 def main():
 
-    userId = sys.argv[1]
-    topicId = sys.argv[2]
-    res = {"userid": userId, "topicid": topicId}
-    print(json.dumps(res))
+    # userId = sys.argv[1]
+    # topicId = sys.argv[2]
+    # res = {"userid": userId, "topicid": topicId}
+    # print(json.dumps(res))
+    print('a')
 
-
-# Start process
+    # Start process
 if __name__ == '__main__':
     main()
